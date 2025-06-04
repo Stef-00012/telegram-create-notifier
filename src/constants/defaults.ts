4620a3c16aefc9707c611f6237ab9d72a4224189
Stef-00012 <admin@stefdp.com>
2025-06-04 11:39:14 +0200

    update defaults

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,8 +1,8 @@
 export const defaultNewAddonMessage = `<blockquote class="tg-blockquote"><b class="tg-bold">New Addon Created</b></blockquote>{{?modrinth:
-<u><b>Modrinth</b></u>
+✦ <u><b>Modrinth</b></u>
 <b class="tg-bold">Name</b>: {{modrinth/name}}
 <b class="tg-bold">Description</b>: {{modrinth/description}}
-<b class="tg-bold">Author</b>: {{modrinth/authorsUrl}}
+<b class="tg-bold">Authors</b>: {{modrinth/authorsUrl}}
 <b class="tg-bold">Versions</b>: {{modrinth/versions}}
 <b class="tg-bold">Creation Date</b>: {{modrinth/created}}
 <b class="tg-bold">Categories</b>: {{modrinth/categories}}
@@ -10,10 +10,10 @@ export const defaultNewAddonMessage = `<blockquote class="tg-blockquote"><b clas
 <b class="tg-bold">Server Side</b>: {{modrinth/serverSide}}
 <b class="tg-bold">Modloaders</b>:  {{modrinth/modloaders}}
 |?}}{{?curseforge:
-<u><b>Curseforge</b></u>
+✦ <u><b>Curseforge</b></u>
 <b class="tg-bold">Name</b>: {{curseforge/name}}
 <b class="tg-bold">Description</b>: {{curseforge/description}}
-<b class="tg-bold">Author</b>: {{curseforge/authorsUrl}}
+<b class="tg-bold">Authors</b>: {{curseforge/authorsUrl}}
 <b class="tg-bold">Versions</b>: {{curseforge/versions}}
 <b class="tg-bold">Creation Date</b>: {{curseforge/created}}
 <b class="tg-bold">Categories</b>: {{curseforge/categories}}
@@ -22,14 +22,14 @@ export const defaultNewAddonMessage = `<blockquote class="tg-blockquote"><b clas
 <b class="tg-bold">Modloaders</b>:  {{curseforge/modloaders}}|?}}`;
 
 export const defaultUpdatedAddonMessage = `<blockquote><b>Addon aggiornato</b></blockquote>{{?modrinth:
-<u><b>Modrinth</b></u>
+✦ <u><b>Modrinth</b></u>
 <b>Nome</b>: {{modrinth/name}}{{modrinth/?addedVersions:
 <b>Versioni (Aggiunte)</b>: {{modrinth/addedVersions}}|?}}{{modrinth/?removedVersions:
 <b>Versioni (Rimosse)</b>: {{modrinth/removedVersions}}|?}}{{modrinth/?addedModloaders:
 <b>Modloaders (Aggiunti)</b>: {{modrinth/addedModloaders}}|?}}{{modrinth/?removedModloaders:
 <b>Modloaders (Rimossi)</b>: {{modrinth/removedModloaders}}|?}}
 |?}}{{?curseforge:
-<u><b>Curseforge</b></u>
+✦ <u><b>Curseforge</b></u>
 <b>Nome</b>: {{curseforge/name}}{{curseforge/?addedVersions:
 <b>Versioni (Aggiunte)</b>: {{curseforge/addedVersions}}|?}}{{curseforge/?removedVersions:
 <b>Versioni (Rimosse)</b>: {{curseforge/removedVersions}}|?}}{{curseforge/?addedModloaders:
