04955f41bbf0360d670c3f3a8613a0d1ed005c21
Stef-00012 <me@stefdp.com>
2025-07-16 18:52:08 +0200

    rm config.ts from gitignore

---
+++ config.ts
@@ -0,0 +1,5 @@
+export const config = {
+	token: process.env.TOKEN as string,
+	createAddonsWSURI: process.env.CREATE_ADDONS_WEBSOCKET_URI as string,
+	ownerIds: process.env.OWNER_IDS?.split(",").map(id => Number.parseInt(id)) as number[],
+};
